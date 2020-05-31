48fd6bf226672e49b073b20bd0bd2cedd0b2ab5f
lenz <lensoff@gmail.com>
2020-05-31 13:04:51 +0300

    adding mac info into network-adapter.ps1

--- vm/network-adapter.ps1
+++ vm/network-adapter.ps1
@@ -5,6 +5,9 @@
 Get-VM | Get-NetworkAdapter | Select-Object @{N="VM";E={$_.Parent.Name}},@{N="NIC";E={$_.Name}},@{N="Network";E={$_.NetworkName}},@{N="IP";E={$_.Parent.Guest.IPAddress | where {([IPAddress]$_).AddressFamily -eq [System.Net.Sockets.AddressFamily]::InterNetwork}}}
 #vm,ip
 Get-VM | Name,@{N="ip";E={$_.Guest.IPAddress | where {([IPAddress]$_).AddressFamily -eq [System.Net.Sockets.AddressFamily]::InterNetwork}}}
+#vm,mac
+Get-VM | Select-Object -Property Name,@{"Name"="MAC";"Expression"={($_ | Get-NetworkAdapter).MacAddress}}
+
 
 #$_.Guest.IPAddress
 #$vm.Guest.IPAddress | where {([IPAddress]$_).AddressFamily -eq [System.Net.Sockets.AddressFamily]::InterNetwork}
